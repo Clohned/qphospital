24a1bbb9f44f823c7058fcdcb6f9cc3e52a103a8
fleevo32 <bmoney456headcrack@gmail.com>
2022-02-18 04:01:23 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'qpr' -- https://github.com/ohqpr
+
+lua54 'yes'
+
+version '1.0.1'
+
+client_scripts {
+  'client/*.lua'
+}
+
+shared_scripts {
+  '@es_extended/imports.lua',
+  'config.lua'
+}
+
+server_scripts {
+  'server/*.lua'
+}
+
+dependencies {
+  'qtarget',
+  'nh-context',
+  'es_extended',
+  'esx_ambulancejob'
+}
