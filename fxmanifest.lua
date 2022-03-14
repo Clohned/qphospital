095d0647a438f37abe06218dffe18bcb1fc00dd4
ohqpr <bmoney456headcrack@gmail.com>
2022-03-14 04:36:42 -0400

    v1.0.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,28 @@
+-- FX Information --
 fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+game       'gta5'
+lua54      'yes'
 
-author 'qpr' -- https://github.com/ohqpr
+-- Resource Information --
+name         'qphospital'
+author       'qpr'
+version      '1.0.4'
+repository   'https://github.com/ohqpr/qphospital'
+description  'Hospital treatment resource utilizing qtarget & nh-context'
 
-version '1.0.3'
-
-client_script 'client.lua'
-server_script 'server/*.lua'
+-- Manifest --
+dependencies {
+  '/server:5181',
+	'/onesync',
+  'qtarget',
+  'nh-context',
+  'esx_ambulancejob'
+}
 
 shared_scripts {
   '@es_extended/imports.lua',
   'config.lua'
 }
 
-dependencies {
-  'qtarget',
-  'nh-context',
-  'es_extended',
-  'esx_ambulancejob'
-}
+client_script 'client.lua'
+server_script 'server/*.lua'
